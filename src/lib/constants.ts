7abb3db570909abd5c906b4821ad938b6628c7c6
雪グリコシド結晶 <IGCyukira@outlook.com>
2025-12-20 14:25:42 +0800

    Add HTTP constants for status and caching

---
+++ src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const HTTPS_REDIRECT_STATUS = 308;
+export const DEFAULT_STATUS = 302;
+export const HSTS_HEADER_VALUE = "max-age=63072000; includeSubDomains";
+export const DEFAULT_CACHE_TTL_SECONDS = 3600;
